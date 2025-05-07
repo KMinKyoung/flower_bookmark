74f7207948244d30ee89472c2b1137ca29c77afe
MINKYOUNG\kyoun <vldzmehowl32@naver.com>
2025-05-07 17:52:34 +0900

    first commit

---
+++ src/main/java/me/minkyoung/flower_bookmark/FlowerBookmarkApplication.java
@@ -0,0 +1,13 @@
+package me.minkyoung.flower_bookmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerBookmarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowerBookmarkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/minkyoung/flower_bookmark/FlowerBookmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package me.minkyoung.flower_bookmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerBookmarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
